034f1b4c9ec87a9058eccd39cb192dbb04655368
Sathya-Srinivasan <72451345+Sathya-Srinivasan@users.noreply.github.com>
2020-11-06 07:16:34 +0530

    Add files via upload

--- OLX_Resale.sql
+++ OLX_Resale.sql
@@ -1,35 +1,63 @@
+####################### START OF SQL SCRIPT ######################																#
+#                                                                #
+# DAY #: 44                                                      #
+#                                                                #
+# THIS SCRIPT IS USED FOR OLX RESALE WHICH MAINTAINS THE DETAILS #
+# ABOUT THE USER AND ROLE AND ALSO ABOUT THE DETAILS OF THE      #
+# PRODUCT ITEMS AND BILL PAYMENT OF THE PRODUCTS                 #
+#                                                                #
+# AUTHOR : SATHYA S                                              #
+#                                                                #
+###################### END OF SQL SCRIPT #########################
+
 CREATE DATABASE OLX_Resale;
 SHOW DATABASES;
 USE OLX_Resale;
+
+/*Creating table user for olx resale*/
 CREATE TABLE user(
-UserId int primary key not null, 
+Id int primary key not null, 
 Name varchar(20) not null, 
-Email varchar(20) not null,
+EmailId varchar(20) not null,
 Address varchar(40) not null,
 Password varchar(15) not null,
-ConfirmPassword varchar(15) not null,
 RoleId int,
-foreign key(RoleId) REFERENCES role(RoleId)
+foreign key(Id) REFERENCES role(Id)
 );
+select * from user;
+ /*create the role table*/
 create table role(
-RoleId int Primary key not null,
-RoleName varchar(20) not null
+Id int Primary key not null,
+RoleName varchar(20) not null,
+foreign key(Id) REFERENCES productItems(Id)
 );
-create table bill_Generation(
-BillId int not null primary key, 
-CustomerName varchar(20) not null,
-StartingDate varchar(12) not null, 
-EndingDate varchar(12) not null,
-Source varchar(20) not null,
-Destination varchar(20) not null,
-TotalCost int not null
-);
+
+/*INSERT INTO role VALUES(1,"CUSTOMER");*/
+
+select * from role;
 create table productItems(
-ProductId int not null primary key,
-Model int not null, Description varchar(100) not null,
+Id int not null primary key,
+Model int not null,
+Description varchar(100) not null,
 Features varchar(100) not null,
 Cost int not null,
 BillId int,
-foreign key(BillId) REFERENCES billpayment(BillId)
+foreign key(Id) REFERENCES billpayment(Id)
 );
+select * from ProductItems;
+/*creating the bill_geneartion table for bill payment of product items*/
+
+create table billpayment(
+Id int not null primary key, 
+CustomerName varchar(20) not null,
+StartingDate varchar(12) not null, 
+EndingDate varchar(17) not null,
+Source varchar(15) not null,
+Destination varchar(18) not null,
+TotalCost int not null
+);
+select * from billpayment;
+
+/*Creating the productItems table for Listing the Products*/
+
 
